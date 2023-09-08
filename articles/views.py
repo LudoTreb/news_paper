481292b833b3822cc945ae0eb57f0218e57d9c47
LudoTreb <l.trebalag@gmail.com>
2023-09-08 14:58:00 +0200

    Separate the ArticleDetailView in two function to manage the view GET and POST for the comments in articles. We have now CommentGet for the get view on ArticleViewDetail and we have the CommentPost for the pos view on ArticleViewDetail

--- articles/views.py
+++ articles/views.py
@@ -1,11 +1,13 @@
 from django.forms.models import BaseModelForm
 from django.http import HttpResponse
-from django.urls import reverse_lazy
-from django.views.generic import DetailView, ListView
+from django.urls import reverse_lazy, reverse
+from django.views.generic import DetailView, ListView, FormView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic.detail import SingleObjectMixin
 
 from .models import Article
+from .forms import CommentForm
 
 
 class ArticleCreateView(LoginRequiredMixin, CreateView):
@@ -26,9 +28,44 @@ class ArticleListView(LoginRequiredMixin, ListView):
     template_name = "article_list.html"
 
 
-class ArticleDetailView(LoginRequiredMixin, DetailView):
+class CommentGet(DetailView):
     model = Article
     template_name = "article_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["form"] = CommentForm()
+        return context
+
+
+class CommentPost(SingleObjectMixin, FormView):
+    model = Article
+    form_class = CommentForm
+    template_name = "article_detail.html"
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        return super().post(request, *args, **kwargs)
+
+    def form_valid(self, form):
+        comment = form.save(commit=False)
+        comment.article = self.object
+        comment.save()
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        article = self.get_object()
+        return reverse("article_detail", kwargs={"pk": article.pk})
+
+
+class ArticleDetailView(LoginRequiredMixin, DetailView):
+    def get(self, request, *args, **kwargs):
+        view = CommentGet.as_view()
+        return view(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        view = CommentPost.as_view()
+        return view(request, *args, **kwargs)
 
 
 class ArticleUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
