6073803115fa75766551d43d416ff3b0defd91e1
LudoTreb <l.trebalag@gmail.com>
2023-09-11 23:53:20 +0200

    install environs and create 'env' for varaible environement

--- django_project/settings.py
+++ django_project/settings.py
@@ -11,10 +11,13 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from environ import Env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+env = Env()
+env.read_env()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
