31783adf76d73652385f93b6c458dc0425850624
LudoTreb <l.trebalag@gmail.com>
2023-09-13 16:37:16 +0200

    config the variable SECRET_KEY ang generate a new one in .env

--- django_project/settings.py
+++ django_project/settings.py
@@ -23,9 +23,7 @@ env.read_env()
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = (
-    'django-insecure-llob%gb7!cgxr1#ga1m^xli&(#=(af337gqv)4sbq6o10$t!0f'
-)
+SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
