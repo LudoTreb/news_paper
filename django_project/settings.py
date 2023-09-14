def519548f913e4a448db3ecb896b0fc71b9e8e3
LudoTreb <l.trebalag@gmail.com>
2023-09-14 16:36:08 +0200

    config the variable DATABASES for production

--- django_project/settings.py
+++ django_project/settings.py
@@ -82,12 +82,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {"defaut": env.dj_db_url("DATABASE_URL")}
 
 
 # Password validation
