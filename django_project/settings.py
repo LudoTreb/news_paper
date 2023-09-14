b6e8f4f154009863ef7968085f5c3bcc3081844a
LudoTreb <l.trebalag@gmail.com>
2023-09-14 16:41:21 +0200

    fix the key 'default' of dict for the DATABASES variable

--- django_project/settings.py
+++ django_project/settings.py
@@ -82,7 +82,7 @@ WSGI_APPLICATION = 'django_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {"defaut": env.dj_db_url("DATABASE_URL")}
+DATABASES = {"default": env.dj_db_url("DATABASE_URL")}
 
 
 # Password validation
